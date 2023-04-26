f72353b15da416a3e81944501ec9d9276c6d6dc9
Thiago Nicoleti <thiagonicoleti88@outlook.com>
2023-04-26 15:27:25 -0300

    Criando um artigo no console do Rails

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_175801) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
