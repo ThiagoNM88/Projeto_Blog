c5281492b54a53f8e004b72f80f75f1f2a4f20ab
Thiago Nicoleti <thiagonicoleti88@outlook.com>
2023-04-29 11:50:21 -0300

    Adicionando lista de índice de artigos e adicionar editar / mostrar links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,37 +1,46 @@
 class ArticlesController < ApplicationController
 
     def show
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
-
+  
     def new
-        @article = Article.new
+      @article = Article.new
     end
-
+  
+    def index
+      @articles = Article.all
+    end
+  
     def create
-        @article = Article.new(article_params)
-        if @article.save
-          flash[:notice] = "Article was successfully created"
-          redirect_to article_path(@article)
-        else
-          render 'new'
-        end
+      @article = Article.new(article_params)
+      if @article.save
+        flash[:notice] = "Article was successfully created"
+        redirect_to article_path(@article)
+      else
+        render 'new'
+      end
     end
-
-    private
-        def article_params
-        params.require(:article).permit(:title, :description)
-    end
-
+  
     def edit
-        @article = Article.find(params[:id])
-        if @article.update(article_params)
-          flash[:notice] = "Article was updated"
-          redirect_to article_path(@article)
-        else
-          flash[:notice] = "Article was not updated"
-          render 'edit'
-        end
+      @article = Article.find(params[:id])
     end
-
-end
+  
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "Article was updated"
+        redirect_to article_path(@article)
+      else
+        flash[:notice] = "Article was not updated"
+        render 'edit'
+      end
+    end
+  
+    private
+  
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+  
+  end
