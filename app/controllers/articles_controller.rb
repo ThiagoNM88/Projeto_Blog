c1d34e1941d67daaf81070a3a508ff5dd6868e34
Thiago Nicoleti <thiagonicoleti88@outlook.com>
2023-05-05 00:01:15 -0300

    Corrigindo Botão Deletar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,14 @@ def create
 end
 def show
 end
+  before_action :require_user, except: [:show, :index]
+
+def require_user
+  if !logged_in?
+    flash[:danger] = "You must be logged in to perform that action"
+    redirect_to login_path
+  end
+end
 def update
   if @article.update(article_params)
    flash[:success] = "Article was updated"
@@ -24,11 +32,15 @@ def update
   end
 end
 def edit
+  @article = Article.find(params[:id])
+  # Adicionar a linha abaixo para adicionar o link para a lista de artigos
+  @articles_path = articles_path
 end
 def index
   @articles = Article.paginate(page: params[:page], per_page: 3)
 end
 def destroy
+  @article = Article.find(params[:id])
   @article.destroy
   flash[:success] = "Article was deleted"
   redirect_to articles_path
